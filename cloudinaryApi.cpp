82928779344a53e0b43a6bbf34f97e5de0b7c67e
Raghav Bahukhandi <raghapb34n@gmail.com>
2024-09-22 21:03:45 +0530

    got pushing to cloudinary api working

--- cloudinaryApi.cpp
+++ cloudinaryApi.cpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <ctime>
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 
@@ -33,45 +34,84 @@ void find_env_value(string key, string &value)
     }
 }
 
-string create_signature(string &string_to_sign)
+string find_hash(string input)
 {
-    string api_secret;
-    find_env_value("API_SECRET", api_secret);
+    unsigned char hash[SHA_DIGEST_LENGTH];
 
-    unsigned char *digest;
+    SHA1(reinterpret_cast<const unsigned char*>(input.c_str()), input.size(), hash);
 
-    digest = HMAC(EVP_sha1(), api_secret.c_str(), api_secret.length(), (unsigned char*)string_to_sign.c_str(), string_to_sign.length(), NULL, NULL);
-    
-    ostringstream result;
-
-    for (int i = 0; i < 20; i++)
-    {
-        result << hex << setw(2) << setfill('0') << (int)digest[i];
+    std::ostringstream hexStream;
+    for (int i = 0; i < SHA_DIGEST_LENGTH; ++i) {
+        hexStream << hex << setw(2) << setfill('0') << static_cast<int>(hash[i]);
     }
 
-    return result.str();
-
+    return hexStream.str(); 
 }
+   
 
 int main()
 {
-    string s = "helloo";
-    cout << create_signature(s) << endl;
+    string api_key;
+    string api_secret;
+    string cloud_name;
+    string image_path = R"(/mnt/c/Users/ragha/projects/quail/IMG_20240813_213153.jpg)";
 
-    return 0;
-
+    find_env_value("CLOUDINARY_API_KEY", api_key);
+    find_env_value("CLOUDINARY_API_SECRET", api_secret);
+    find_env_value("CLOUDINARY_CLOUD_NAME", cloud_name);
+    
+    time_t timestamp  = time(0);
+    cout << api_secret << endl;
+    ostringstream string_to_sign;
+    string_to_sign << "timestamp=" << timestamp << api_secret;
+    cout << timestamp << endl;
+    string signature = find_hash(string_to_sign.str());
+    cout << signature << endl;
 	CURL *curl = curl_easy_init();
-
+    CURLcode res;
 	if (curl)
 	{       
-        cout << "Inside curl" << endl;
-	    CURLcode res;
-        curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.6:3000/");
-		res = curl_easy_perform(curl);
-        cout << curl_easy_strerror(res) << endl;
-        cout << CURLOPT_URL;
-		curl_easy_cleanup(curl);
+        string url = "https://api.cloudinary.com/v1_1/" + cloud_name + "/image/upload";
+
+        struct curl_httppost *form = NULL;
+        struct curl_httppost *lastptr = NULL;
+
+        curl_formadd(&form, &lastptr,
+                     CURLFORM_COPYNAME, "file",
+                     CURLFORM_FILE, image_path.c_str(),
+                     CURLFORM_END);
+        
+        curl_formadd(&form, &lastptr,
+                     CURLFORM_COPYNAME, "api_key",
+                     CURLFORM_COPYCONTENTS, api_key.c_str(),
+                     CURLFORM_END);
+
+        curl_formadd(&form, &lastptr,
+                     CURLFORM_COPYNAME, "timestamp",
+                     CURLFORM_COPYCONTENTS, to_string(timestamp).c_str(),
+                     CURLFORM_END);
+
+        curl_formadd(&form, &lastptr,
+                     CURLFORM_COPYNAME, "signature",
+                     CURLFORM_COPYCONTENTS, signature.c_str(),
+                     CURLFORM_END);
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPPOST, form);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
+        } else {
+            cout << "Image uploaded successfully!" << endl;
+        }
+
+        curl_easy_cleanup(curl);
+        curl_formfree(form);
+
 	}
 
 	return 0;
+
 }

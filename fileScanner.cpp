b356026c0af9ab9f8c9f5faeeae09b48f2d483b7
Raghav Bahukhandi <raghapb34n@gmail.com>
2024-09-25 22:10:42 +0530

    added code to read and update files

--- fileScanner.cpp
+++ fileScanner.cpp
@@ -39,6 +39,22 @@ void findImgId(string line, string *emptyId)
 
 }
 
+void replaceImgSrc(string line, string newSrc, string &newLine)
+{
+	regex pattern(R"(<img\b[^>]*\bsrc\s*=\s*['"]([^'"]+)['"])");
+
+	smatch match;
+
+	if(regex_search(line, match, pattern)) {
+		regex replacementPattern(match[1].str());
+
+		string newString = regex_replace(line, replacementPattern, newSrc);
+		for(string::iterator it = newString.begin(); it != newString.end(); it++)
+			newLine.push_back(*it);
+	}
+	return;	
+}
+
 int main()
 {
     directory_iterator d = directory_iterator(current_path());
@@ -46,31 +62,41 @@ int main()
     cout << "Looking for image tags with the id `test` in the directory: " << current_path() << endl << endl;
     for(d; d != e; d++)
     {
-	    string relativePath = relative(d->path());
+	    string relativePath = relative(d->path()).string();
 	    if ((includes(relativePath, ".") == 0) || relativePath[0] == *".")
 	    {
 	        continue;
 	    }
 	    cout<< "Checking file " << relativePath << endl;
-	
 	    string line;
-	    string pathname = d->path();
+	    string pathname = d->path().string();
 	    ifstream file = ifstream(pathname);
-	
+
 	    while (getline(file, line))
 	    {
+            int currentPos = file.tellg();
             if (isImgTag(line))
 	        {
                 string imageId;
                 findImgId(line, &imageId);
                 if (imageId == "test") 
-                {    
-                    cout << "Found image tag with the `test` id:" << endl << line << endl << endl;
+                {
+                    file.close();
+                    ofstream writeFile(pathname, ios_base::app);
+                    writeFile.seekp(currentPos);
+                    string newLine;
+                    string replacement = "test2.jpg";
+                    replaceImgSrc(line, replacement, newLine);
+                    writeFile.write(newLine.c_str(), newLine.size());
+                    writeFile.write("\n", 1);
+                    int newPos = writeFile.tellp();
+                    writeFile.close();
+                    file = ifstream(pathname);
+                    file.seekg(newPos);
+                    cout << "Updated file " << pathname << endl;
                 }
-
             }
-	    }
-
+        }
 	    cout << endl;
     }
     return 0;
